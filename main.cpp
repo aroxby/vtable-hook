5721a73eaa9c1b1fd020f30a72ce1fa5e81ac587
Andy Roxby <aroxby@users.noreply.github.com>
2019-09-02 13:00:35 -0400

    ITS ALIVE!!!!!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <cstdio>
+#include <cstring>
 
 class Victim {
 public:
@@ -31,23 +32,37 @@ public:
     }
 };
 
-int thisfn(void *self, void *vp) {
-    // return printf("%p.tn(%p)\n", self, vp);
-    return 0;
+void install_new_vtable(void *obj, int num_members = 1) {
+    const static size_t sz = num_members * sizeof(void*);
+    VTable *old_table = (VTable*)obj;
+    VTable new_table;
+    new_table.vtbl = new const void*[num_members / sizeof(void*)];
+    memcpy(new_table.vtbl, old_table->vtbl, sz);
+    memcpy(obj, &new_table.vtbl, sizeof(new_table.vtbl));
+}
+
+int thisfn(void *vp) {
+    return printf("%s.tn(%p)\n", "[]", vp);
 }
 
 int main() {
-    Victim v;
     setbuf(stdout, NULL);
+    Victim *v = new Victim;
+    VTable *t;
 
-    v.fn((void*)0xBEEF);
+    v->fn((void*)0xBEEF);
 
-    VTable *t = (VTable*)(&v);
-    printf("%I64p = %p\n", &v.fn, t->get(&v.fn));
+    t = (VTable*)(v);
+    printf("%I64p = %p\n", &v->fn, t->get(&v->fn));
 
-    t->set(&v.fn, &thisfn);
-    printf("%I64p = %p\n", &v.fn, t->get(&v.fn));
-    //v.fn((void*)0xCAFE);
+    install_new_vtable(v);
+    t = (VTable*)(v);
+    printf("%I64p = %p\n", &v->fn, t->get(&v->fn));
 
+    t->set(&v->fn, &thisfn);
+    printf("%I64p = %p\n", &v->fn, t->get(&v->fn));
+    int r = v->fn((void*)0xCAFE);
+
+    printf("Done, %s\n", r==16?"T":"F");
     return 0;
 }
